690fa51226adfd72d6bbef184896a137c98a5009
Lemarius <luxcruentis@gmail.com>
2023-06-22 12:34:05 +0200

    UserListv3

--- src/components/userlist/UserList.jsx
+++ src/components/userlist/UserList.jsx
@@ -19,25 +19,43 @@ import {
 	StyledSelect,
 	StyledTextCont
 } from './styles';
+import { useState } from 'react';
 
 const UserList = () => {
 	const navigate = useNavigate();
+
+	const [onlyActive, setOnlyActive] = useState(false);
+	const [searchText, setSearchText] = useState('');
+	const [sortBy, setSortBy] = useState(0);
+
+	const filteredUsers = filterUserByActive(onlyActive, searchText, sortBy);
+
+	console.log(onlyActive);
 
 	return (
 		<StyledContainer>
 			<StyledH1>Listado de usuarios</StyledH1>
 			<StyledFilterCont>
-				<StyledInput />
+				<StyledInput
+					value={searchText}
+					onChange={e => handleInput(setSearchText, e)}
+				/>
 				<StyledDiv>
 					<StyledH2>Sólo activos:</StyledH2>
-					<StyledCheckbox type='checkbox' />
+					<StyledCheckbox
+						type='checkbox'
+						onChange={() => handleCheckbox(onlyActive, setOnlyActive)}
+					/>
 				</StyledDiv>
-				<StyledSelect>
-					<StyledOption value='Por defecto'>Por defecto</StyledOption>
-					<StyledOption value='Por nombre'>Por nombre</StyledOption>
+				<StyledSelect
+					value={sortBy}
+					onChange={e => setSortBy(Number(e.target.value))}
+				>
+					<StyledOption value={0}>Por defecto</StyledOption>
+					<StyledOption value={1}>Por nombre</StyledOption>
 				</StyledSelect>
 			</StyledFilterCont>
-			{USERS.map(user => (
+			{filteredUsers.map(user => (
 				<StyledInfoCont key={user.userId}>
 					<StyledImgCont>
 						<StyledImg src={user.profileImage} />
@@ -58,6 +76,40 @@ const UserList = () => {
 			))}
 		</StyledContainer>
 	);
+};
+
+const filterUserByActive = (onlyActive, searchText, sortBy) => {
+	let filteredUsers = [...USERS];
+	filteredUsers = onlyActive
+		? filteredUsers.filter(user => user.active)
+		: filteredUsers;
+	if (searchText) {
+		filteredUsers = filteredUsers.filter(user =>
+			user.name.toLowerCase().includes(searchText.toLowerCase())
+		);
+	}
+
+	if (sortBy === 1) {
+		filteredUsers.sort((a, b) => {
+			if (a.name < b.name) {
+				return -1;
+			}
+			if (a.name > b.name) {
+				return 1;
+			}
+			return 0;
+		});
+	}
+
+	return filteredUsers;
+};
+
+const handleCheckbox = (onlyActive, setOnlyActive) => {
+	setOnlyActive(!onlyActive);
+};
+
+const handleInput = (setSearchText, e) => {
+	setSearchText(e.target.value);
 };
 
 export default UserList;
